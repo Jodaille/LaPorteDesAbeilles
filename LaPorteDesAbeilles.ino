8e9e12f00a83a7af9e23b890da6e2a1cbc52101c
Jodaille <jody.noury@gmail.com>
2015-11-25 22:00:38 +0100

    au moins deux fonctions nécessaires: setup() et loop()

--- LaPorteDesAbeilles.ino
+++ LaPorteDesAbeilles.ino
@@ -1,9 +1,22 @@
+/**
+ * La fonction setup() est appelée
+ * au démarrage d'Arduino
+ */
 void setup() {
-  // put your setup code here, to run once:
+    // les instructions ci-dessous
+    // entre les deux accolades {}
+    // seront éxécutées à chaque démarrage
 
 }
 
+/**
+ * La fonction loop() est appelée
+ * aussitôt après setup() et en "boucle"
+ * à la fréquence de 25Mhz soit 25000 fois
+ * par secondes
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
+  // le code sera répété tant que les ressources
+  // en électricité et mémoire seront disponibles
 
 }
