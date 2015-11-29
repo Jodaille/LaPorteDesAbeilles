15208c08082634cde84ade4e3cb11d42aa864542
Jodaille <jody.noury@gmail.com>
2015-11-29 11:37:56 +0100

    Utisation des variables pour stocker le pin utilisé et la valeur lue

--- LaPorteDesAbeilles.ino
+++ LaPorteDesAbeilles.ino
@@ -1,22 +1,38 @@
 /*
+ * Nous utilisons une variable pour stocker
+ * le numéro du pin sur lequel nous branchons
+ * le capteur infra rouge.
+ * nous utilisons une constante avec const
+ * et notre variable sera un entier int
+ */
+const int passageA = A0;
+
+/*
+ * pour la mesure de réflexion du capteur
+ * nous utiliserons aussi une variable de type int
+ */
+int reflexion1;
+
+/*
 * La fonction setup() est appelée
 * au démarrage d'Arduino
 */
 void setup() {
-  // les instructions ci-dessous 
-  // entre les deux accolades {} 
+  // les instructions ci-dessous
+  // entre les deux accolades {}
   // seront éxécutées à chaque démarrage
 
   // Nous activons la liaison série
   Serial.begin(9600);
 
   // Nous émettons un message :
-  Serial.println("Hello world !");
+  Serial.println("Starting setup");
 
-  Serial.println("A0 a pour valeur:");
-  Serial.println(analogRead(A0));
+  reflexion1 = analogRead(passageA);
+  Serial.println("A0/passageA a pour valeur:");
+  Serial.println(reflexion1);
 
-  
+
 }
 
 /*
@@ -29,6 +45,6 @@ void loop() {
   // le code sera répété tant que les ressources
   // en électricité et mémoire seront disponibles
   Serial.print("A0 = ");
-  Serial.println(analogRead(A0));
+  Serial.println(analogRead(passageA));
 
 }
